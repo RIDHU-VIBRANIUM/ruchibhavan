505f1aa7c31df7dfff2454d4d012dae0a601554b
Ridhu__Vibranium <mujeebridhan@gmail.com>
2021-07-07 22:26:35 +0400

    full completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
+(function () {
 
-
+    var navButton = document.querySelector("#nav-menu-button");
+    var navUl = document.querySelector(".nav-ul");
+  
+    function toggleMobileMenu() {
+      navUl.classList.toggle("hide-ul");
+    }
+  
+    navButton.onclick = toggleMobileMenu;
+  }());
+  
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
@@ -44,7 +54,7 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal(`.home__container_bd-container_bd-grid,.map,
+sr.reveal(`.home__container_bd-container_bd-grid,
             .about__data, .about__img,
             .services__content, .menu__content,
             .app__data, .app__img,
